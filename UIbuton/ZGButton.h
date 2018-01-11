755ed2201c620754c503e104daa35a3b5655f961
zhangzhi0903 <18310127735@163.com>
2018-01-11 10:54:55 +0800

    提交代码

---
+++ UIbuton/ZGButton.h
@@ -0,0 +1,13 @@
+//
+//  ZGButton.h
+//  UIbuton
+//
+//  Created by offcn_zcz32036 on 2017/12/22.
+//  Copyright © 2017年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZGButton : UIButton
+
+@end
---
+++ UIbuton/ZGButton.m
@@ -0,0 +1,21 @@
+//
+//  ZGButton.m
+//  UIbuton
+//
+//  Created by offcn_zcz32036 on 2017/12/22.
+//  Copyright © 2017年 cn. All rights reserved.
+//
+
+#import "ZGButton.h"
+
+@implementation ZGButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
